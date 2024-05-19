c1d6e47a63621382ee470553e51a922106c5b22a
emersonjps <emerson20010213@gmail.com>
2024-05-18 21:09:03 -0300

    bugfic - CORS

--- src/main/java/com/desafio/dio/desafiodio/DesafioDioApplication.java
+++ src/main/java/com/desafio/dio/desafiodio/DesafioDioApplication.java
@@ -4,7 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
 @EnableFeignClients
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class DesafioDioApplication {
 
